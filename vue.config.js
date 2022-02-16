6aa29b73940a33503de30e3d0b157649e3aee509
Zé Máximo <torkpuppet@gmail.com>
2022-02-16 18:31:10 -0300

    as

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/ze/" : "/",
-  };
+    publicPath: '/ze',
+    outputDir: 'docs',
+    filenameHashing: false,
+    productionSourceMap: false,
+  }
